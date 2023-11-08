d62b773a0c4c48c2be026389e44d71856f4321ce
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-11-08 12:24:54 -0500

    adding first infrastrcture file.
    creates Jenkins, Docker, and terraform instances

---
+++ first-infrastucture/main.tf
@@ -0,0 +1,70 @@
+
+# configure aws provider
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "us-east-1"
+}
+
+
+
+# create instance
+resource "aws_instance" "jenkins_man" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [var.defaultsg]
+  key_name = "d8"
+
+  user_data = "${file("deployjenkins.sh")}"
+
+  tags = {
+    "Name" : "d8_Jenkins_instance"
+  }
+
+}
+
+# create instance
+resource "aws_instance" "terraform" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [var.defaultsg]
+  key_name = "d8"
+  user_data = "${file("deployterraform.sh")}"
+
+  tags = {
+    "Name" : "d8_Terraform"
+  }
+
+}
+
+# create instance
+resource "aws_instance" "docker" {
+  ami = var.ami
+  instance_type = "t2.medium"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [var.defaultsg]
+  key_name = "d8"
+  user_data = "${file("deploydocker.sh")}"
+
+  tags = {
+    "Name" : "d8_Docker"
+  }
+
+}
+
+
+output "instance_ip" {
+  value = aws_instance.jenkins_man.public_ip
+
+}
+
+output "instance_ip2" {
+  value = aws_instance.terraform.public_ip
+}
+
+output "instance_ip3" {
+  value = aws_instance.docker.public_ip
+}
